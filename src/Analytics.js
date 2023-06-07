ab187e4cc5d5a642983892f0a0e9386e37b8e557
Kristian Sølvberg <kristian@wtw.no>
2023-06-07 09:24:47 +0200

    Added document title variable to dynamic page load event

--- src/Analytics.js
+++ src/Analytics.js
@@ -5,9 +5,10 @@ export default function WithAnalytics(props){
 	let location = useLocation();
 
 	useEffect(() => {
+		console.log('Dynamic route change: ', props.title);
 		document.title = props.title;
-		console.log('Dynamic page change: ', props.title);
-		window._mtm.push({"event": "dynamic-page-load"});
+		//window._paq.push(['setDocumentTitle', props.title]);
+		window._mtm.push({"event": "dynamic-page-load", "documentTitle": props.title});
 
 	}, [location, props.title]);
 
