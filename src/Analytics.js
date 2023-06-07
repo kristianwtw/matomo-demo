6da5bb43a0fba78803fefbacb8fbad1cbd34f523
Kristian Sølvberg <kristian@wtw.no>
2023-06-07 11:04:53 +0200

    Changed dynamic pageview trigger name

--- src/Analytics.js
+++ src/Analytics.js
@@ -5,10 +5,10 @@ export default function WithAnalytics(props){
 	let location = useLocation();
 
 	useEffect(() => {
-		console.log('Dynamic route change: ', props.title);
 		document.title = props.title;
-		//window._paq.push(['setDocumentTitle', props.title]);
-		window._mtm.push({"event": "dynamic-page-load", "documentTitle": props.title});
+
+		// Push an event to Matomo, triggering a page view
+		window._mtm.push({"event": "dynamic-page-view", "documentTitle": props.title});
 
 	}, [location, props.title]);
 
